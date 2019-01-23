eb6ae63f3d0f18a44493028dfb43dbf2b163a6a5
kongzz311 <kozenzei@outlook.com>
2019-01-24 01:04:42 +0800

    first

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.get('/', function(req, res) {
-  res.render('home', {})
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(3000, function() {
